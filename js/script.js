5af23b385e7fa4277e0154623808f265feaea54c
Lucas Sanches <sancheseducacional@gmail.com>
2025-04-21 20:47:35 -0300

    :wrench: Corrigindo o código para exibir o menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-// Seleção do ícone do hambúrguer 
 const hamburgerBtn = document.getElementById("hamburger-btn");
 const navMenu = document.getElementById("nav-menu");
 
-// Quando o ícone do hambúrguer for clicado, alterna a visibilidade do menu
-hamburgerBtn.addEventListener("click", function() {
-    navMenu.classList.toggle("visible");
+// Alterar a visibilidade
+hamburgerBtn.addEventListener("click", function () {
+  navMenu.classList.toggle("visible");
+});
+
+// Fechar o menu após clicar em um lino
+document.querySelectorAll('#nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('visible');
+  });
 });
